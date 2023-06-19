c3966f314bd16a813f9fbd74c439005af8016ba0
Christian <36796016+ChrstnAnsl@users.noreply.github.com>
2023-06-19 10:56:25 +0800

    update value and comment

--- workshop_day_1/Exercise/Exercise.java
+++ workshop_day_1/Exercise/Exercise.java
@@ -3,7 +3,8 @@ package workshop_day_1.Exercise;
 public class Exercise {
 
     /**
-     * Create a chain method for a "Person" that converts the firstName and lastName
+     * Create a chain method for a "Person" that converts the first letter of
+     * firstName and lastName
      * to as capital letter. Set the birthdate to be parameterized to get your age
      * dynamically to be based on your date of birth. Validate the age it should be
      * 18 and above as legal age
@@ -14,9 +15,9 @@ public class Exercise {
     public static void main(String[] args) {
 
         Person person = new Person()
-                .setFirstName("ansel").validateString()
+                .setFirstName("ansel sample").validateString()
                 .setLastName("workshop").validateString()
-                .setBirthDate(2023, 01, 01);
+                .setBirthDate(2001, 01, 01);
 
         person.introduce(); // Get your firstName, lastName and Age
     }
